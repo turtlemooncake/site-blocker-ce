c881bff52a43aa878966521e4d129c80dc0269b8
Victoria Li <victoriali@Victorias-MacBook-Pro.local>
2024-07-30 00:06:41 -0700

    comments + clean

--- scripts/background.js
+++ scripts/background.js
@@ -1,11 +1,9 @@
-
-// Event Listener to tab update
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//     siteBlocker(tabId, changeInfo, tab)
-// });
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => siteBlocker(tabId, changeInfo, tab));
+// Vars
 const HOME_URL = "https://www.google.com/"; 
 const BANNED_URLS = ["youtube", "instagram"] 
+
+// Listener
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => siteBlocker(tabId, changeInfo, tab));
 
 // Evaluates current tab's url; redirects away from certain URLs 
 async function siteBlocker(tabId, changeInfo, tab){
@@ -17,7 +15,7 @@ async function siteBlocker(tabId, changeInfo, tab){
     try {
         let currUrl = tab.url; 
         if (BANNED_URLS.some(url => currUrl.toLowerCase().includes(url))){
-            chrome.tabs.update(tabId, { url: HOME_URL }); // or undefined instead of tab id; we'll double check
+            chrome.tabs.update(tabId, { url: HOME_URL }); 
         }
     } catch (err) {
         console.log("Thrown in onUpdated try/cetch. Error: %s", err)
